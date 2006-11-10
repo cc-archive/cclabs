20319dbd0a3c93a5a77cb4370dd4ecef6a4a6f10
kidproto <kidproto@committer.creativecommons.org>
2006-11-10 01:21:41 +0000

    Gettextified text

--- licensechooser/index.php
+++ licensechooser/index.php
@@ -1,5 +1,6 @@
 <?php
-    $pagetitle  = 'CC License Chooser';
+    $pagetitle  = _('CC License Chooser');
+    $include    = 'style.css';
     $head_extra = 
 	'<script type="text/javascript" language="javascript" src="prototype.js"></script>
 	<script type="text/javascript" language="javascript" src="tooltip.js"></script>
@@ -18,23 +19,38 @@
 	<div id="content">
 		<div id="main">
 			<div class="block">
-			<h2>Choose a License</h2>
-            <p>With a Creative Commons license, <strong>you keep your copyright</strong> but allow people to <a href="http://creativecommons.org/learn/licenses/fullrights">copy and distribute your work</a> provided they <a href="http://creativecommons.org/characteristic/by?lang=en" onclick="window.open('http://creativecommons.org/characteristic/by?lang=en', 'characteristic_help', 'width=375,height=300,scrollbars=yes,resizable=yes,toolbar=no,directories=no,location=yes,menubar=no,status=yes');return false;">give you credit</a> -- and only on the conditions you specify here. For those new to Creative Commons licensing, we've prepared <a href="http://creativecommons.org/about/think">a list of things to think about</a>. If you want to offer your work with no conditions, choose the <a href="http://creativecommons.org/publicdomain-2">public domain</a>.</p>
+			<h2><?= _('Choose a License'); ?></h2>
+            <p>
+            <?= sprintf(_('With a Creative Commons license, <strong>you keep your copyright</strong> but allow people to %scopy and distribute your work%s provided they %sgive you credit%s &mdash; and only on the conditions you specify here.'), '<a href="http://creativecommons.org/learn/licenses/fullrights">', '</a>', '<a href="http://creativecommons.org/characteristic/by?lang=en" onclick="window.open(\'http://creativecommons.org/characteristic/by?lang=en\', \'characteristic_help\', \'width=375,height=300,scrollbars=yes,resizable=yes,toolbar=no,directories=no,location=yes,menubar=no,status=yes\');return false;">', '</a>') . sprintf(_("For those new to Creative Commons licensing, we've prepared %sa list of things to think about%s."), '<a href="http://creativecommons.org/about/think">', '</a>') . sprintf(_('If you want to offer your work with no conditions, choose the %spublic domain%s'), '<a href="http://creativecommons.org/publicdomain-2">', '</a>') ?>
+            </p>
 			</div>
 			
 			<div id="lic-menu" class="block">
-				<h3>Choose the options below for your personalized Creative Commons license.</h3>
+				<h3><?= _('Choose the options below for your personalized Creative Commons license.'); ?></h3>
 				<form>
 					<p>
-						<em><span id="by">by</span><span id="nc">-nc</span><span id="nd">-nd</span><span id="sa" style="display:none;">-sa</span></em><br/>
-					
 					<input type="checkbox" onChange="modify(this)" name="com" value="" id="com" />
-					<label for="com"><strong>Allow commercial uses of your work?</strong></label><br />
+					<label for="com"><strong><?= _('Allow commercial uses of your work?'); ?></strong></label><br />
 					<input type="checkbox" onChange="modify(this)" name="mod" value="" id="mod" />
-					<label for="mod"><strong>Allow modifications of your work?</strong></label><br />
+					<label for="mod"><strong><?= _('Allow modifications of your work?'); ?></strong></label><br />
 					<input type="checkbox" name="share" onChange="modify(this)" value="" id="share" disabled />
-					<label for="share" id="share-label" class="inactive"><strong>Require other people to share their changes?</strong></label><br />
+					<label for="share" id="share-label" class="inactive"><strong><?= _('Require other people to share their changes?'); ?></strong></label><br />
 					</p>
+
+                    <h4><?= _('Where are you going to apply this license?') ;?></h4>
+					<p>
+                    <input type="radio" onChange="modify(this)" name="used" value="" id="use_webpage" />
+					<label for="used_webpage"><?= _('Webpage'); ?></label> 
+					<input type="radio" onChange="modify(this)" name="used" value="" id="used_myspace" />
+					<label for="use_myspace"><?= _('Myspace'); ?></label><br />
+                    </p>
+
+                    <h4><?= _('Currently Selected License'); ?></h4>
+                    <div id="license_selected">
+						<em><span id="by">by</span><span id="nc">-nc</span><span id="nd">-nd</span><span id="sa" style="display:none;">-sa</span></em><br/>
+
+                    </div>
+
 					<!-- MySpace centric style module. You know, for the kids. 
 					<p> 
 						<strong>Style</strong><br />
@@ -52,11 +68,11 @@
 						<label onmouseover="doTooltip(event,'profile.png')" onmouseout="hideTip()"><input type="radio" name="pos" value="fixed" id="pos_fixed" /> Profile&nbsp;</label>
 					</p> -->
 					
-					<p><input type="button" name="submit" value="Create!" id="submit" onClick="testSub();"/></p>
+					<p><input type="button" name="submit" value="<?= _('Create Code!'); ?>" id="submit" onClick="testSub();"/></p>
 					
 					<!-- <p id="lic-result">
 						Now paste the following code into your <em>Who I'd Like To Meet</em> box. It may look scary and confusing, but don't worry, it's full of useful information that keeps your creativity free!<br/> -->
-                       <p id="lic-result">Paste the following code into your web page's html.<br />
+                       <p id="lic-result"><?= _("Paste the following code into your web page's html."); ?><br />
 						<textarea name="result" id="result" rows="8" cols="60">	</textarea>
 					</p>
 				</form>
