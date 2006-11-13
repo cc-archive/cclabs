29a4b8a66810428f89638d050e3b5fcc71421113
bse <bse@committer.creativecommons.org>
2006-11-13 23:09:46 +0000

    Support CC

--- _footer.php
+++ _footer.php
@@ -1,6 +1,7 @@
     </div>
     <div id="footer">
-      <a href="http://creativecommons.org/">Creative Commons</a> | <a href="http://creativecommons.org/contact">Contact</a>
+<a href="http://creativecommons.org/support/"><img src="/images/spread-3.gif" style="margin-bottom:20px;" align="right" border="0" alt="Support CC 2006"/></a>    
+  <a href="http://creativecommons.org/">Creative Commons</a> | <a href="http://creativecommons.org/contact">Contact</a>
     </div>
   </div>
 </body>
