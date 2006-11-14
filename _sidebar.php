885ccab744cc2311eff41a454cab5ce78d640a42
kidproto <kidproto@committer.creativecommons.org>
2006-11-14 05:57:25 +0000

    Added sidebar

---
+++ _sidebar.php
@@ -0,0 +1,36 @@
+<div id="sidebar">
+
+<h3>Current Projects</h3>
+
+<ul>
+<li>Freedoms Licensing Engine <a href="/remixshare/one.php">1</a>, 
+<a href="/remixshare/two.php">2</a>, <a href="/remixshare/three.php">3</a>, 
+<a href="/remixshare/four.php">4</a>, <a href="/remixshare/five.php">5</a></li>
+<li><a href="/licensechooser/">DHTML License Chooser</a></li>
+</ul>
+
+<h3>Explore</h3>
+
+<p>Code for the labs projects may be found in our 
+<a href="http://sf.net/projects/cctools">Sourceforge project page</a>. 
+We encourage you to <a href="http://wiki.creativecommons.org/SVN">install this code</a>, <a href="http://wiki.creativecommons.org/HOWTO_Patch">create patches</a> for the edits you make, <a href="http://sourceforge.net/tracker/?group_id=80503&atid=559968">submit them to our patch tracker</a>, and 
+actively participate in the development of our labs projects.</p>
+
+<h3>Participate</h3>
+
+<p>Please join the <a href="http://developer.creativecommons.org/">CC Developer Community</a> by participating on the following three communication channels.
+</p>
+
+<ul>
+<li><a href="http://wiki.creativecommons.org/IRC">Chat Channel</a>: Log onto 
+#cc on irc.freenode.net.</li>
+<li><a href="http://wiki.creativecommons.org/Mailing_Lists">Mailing lists</a>:
+<ul><li><a href="http://lists.ibiblio.org/mailman/listinfo/cc-devel">cc-devel</a></li>
+<li><a href="http://lists.ibiblio.org/mailman/listinfo/cc-metadata">cc-metadata</a
+></li></ul>
+</li>
+<li><a href="http://wiki.creativecommons.org/wiki/Developer">Developer Wiki</a></li>
+</ul>
+
+</div>
+
