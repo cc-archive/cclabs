a57d9021751e0294c0684cd0e9e68bc1513cec33
mlinksva <mlinksva@committer.creativecommons.org>
2006-11-28 21:05:49 +0000

    new license button

--- _footer.php
+++ _footer.php
@@ -1,9 +1,9 @@
     </div>
     <div id="footer">
 <?php include_once '_sidebar.php'; ?>
-<p><a href="http://creativecommons.org/licenses/by/2.5/"><img src="http://creativecommons.org/images/public/somerights20.png" border="0" alt="Creative Commons" /></a><br />
+<p><a href="http://creativecommons.org/licenses/by/2.5/"><img src="http://i.creativecommons.org/l/by/2.5/88x31.png" border="0" alt="Creative Commons" /></a><br />
 Except where otherwise <a href="http://creativecommons.org/policies#license">noted</a>, content on this site is<br />
-licensed under a <a href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5 License</a></p>
+licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5 License</a></p>
 
   <p><a href="http://creativecommons.org/">Creative Commons</a> | <a href="http://creativecommons.org/contact">Contact</a></p>
     </div>
