55dedaab73f47f2c148df919164d566bc8ef88a9
mlinksva <mlinksva@committer.creativecommons.org>
2006-12-03 19:47:27 +0000

    correct link

---
+++ metadata/demo-beyond.php
@@ -0,0 +1,20 @@
+<?php 
+    $pagetitle = "Metadata Lab Demo";
+    include_once "../_header.php"; 
+?>
+
+<div id="content">
+<h2>Metadata Lab Demo</h2>
+
+<p>If this were not a demo this would be a page where you could obtain commercial use rights, buy related media, make a donation, or establish the provenance of the linked item.</div>
+
+<p>Computers can discover this link via extended work metadata. We're working with several organizations to exploit this technology.</p>
+
+<p><a href="./">Back to the demo.</a></p>
+
+</div>
+
+<?php 
+    include_once "../_footer.php"; 
+?>
+
