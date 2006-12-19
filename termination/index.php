8130df3303e8d556129dc295a441eaa0199cfe7d
dpk <dpk@committer.creativecommons.org>
2006-12-19 19:58:25 +0000

    Initial import of termination of transfer code.

---
+++ termination/index.php
@@ -0,0 +1,110 @@
+<? $pagetitle = "CC Termination of Transfer Tool"; ?>
+<? $head_extra = '<link rel="stylesheet" href="includes/style.css" type="text/css" />
+
+
+    <script type="text/javascript" src="includes/prototype.js"></script>
+    <script type="text/javascript" src="includes/domready.js"></script>
+    <script type="text/javascript" src="includes/scriptaculous.js?load=effects"></script>
+    <script type="text/javascript" src="includes/main.js"></script>
+    <script type="text/javascript" src="includes/lib/XML/ObjTree.js"></script>
+
+<script type="text/javascript">
+      //<![CDATA[
+      Event.onDOMReady(fixGlossaryLinks);
+      //]]>
+    </script>' ?>
+
+<? include_once "../_header.php"; ?>
+
+<div id="content">
+  <h2>Returning Author's Rights: The CC Termination of Transfer Tool</h2>
+
+<div id="totnav">
+
+<div id="overview">
+<h4>Overview</h4>
+<a href="#how_can_it_assist">How Can This Tool Assist You?</a><br>
+<a href="#what_does_it_mean">What Does This Mean For You?</a><br>
+<a href="#what_does_it_do">What Does This Tool Do?</a><br>
+<a href="#get_started">How Do I Get Started?</a><br>
+</div>
+
+<div id="termlist">
+  <h4>Legal Concepts</h4>
+</div>
+
+</div>
+
+<div id="intro">
+  
+<p>Welcome to the Creative Commons Termination of Transfer Tool.  On this page we briefly explain what
+termination of transfer is all about, and how this tool can work for you.  If you like exploring as much
+as we do, try the navigation links over there to the left and at the bottom of the page.  And don't be scared
+to click on scary looking legal terms; we've put definitions for you over in our glossary.
+
+<h3 id="how_can_it_assist">How can this tool assist you?</h3>
+
+<p>Are you the creator of a book, a cartoon, a screenplay, a movie, artwork, or some other
+type of creative <a class="glossarylink" href="glossary.html#work">work</a> that is protected by
+<a class="glossarylink" href="glossary.html#copyright_law">copyright law</a>?<br>
+<br>
+Are you a <a class="glossarylink" href="glossary.html#descendants">spouse, widow, other relative or authorized
+representative</a> of an <a class="glossarylink" href="glossary.html#author">author or artist</a>?<br>
+<br>
+Did you or the <a class="glossarylink" href="glossary.html#author">author or artist</a> enter into
+an <a class="glossarylink" href="glossary.html#agreement">agreement or transfer</a> in relation to that work with
+someone else who is now reaping commercial benefits from your or the author or artist's
+hard work and you would now like to share in the benefits?</p>
+
+<p>If so, this tool may assist you to terminate that agreement or transfer.  Terminating
+the agreement means that the rights in the work return to you, and you are again in control
+of all of the rights in relation to your work.</p>
+
+<p>To learn more about this tool and whether it might be useful for you, feel free to check out
+our <a href="faq.html">FAQ</a>.</p>
+
+<h3 id="what_does_it_mean">What does this mean for you?</h3>
+
+<p>Under US law, <a class="glossarylink" href="glossary.html#author">authors and artists</a> and, in some cases, their
+<a class="glossarylink" href="glossary.html#descendants">spouse, widow, other relatives or authorized representatives</a>
+have the chance to terminate earlier <a class="glossarylink" href="glossary.html#agreement">agreements or transfers</a>
+that they may have entered into many years ago before they
+knew how successful or profitable their work would be.  Under certain provisions of the
+US Copyright Act, commonly called the "termination of transfer" provisions,
+authors and artists, and in some cases,
+their spouse, widow, other relatives or authorized representatives
+can terminate these agreements and reclaim their US rights.
+They can then negotiate new licensing and other deals with the strength of the knowledge of
+market value of their work.</p>
+
+<h3 id="what_does_it_do">What does this tool do?</h3>
+
+<p>This tool is designed to make it easier for you to exercise your rights to terminate
+<a class="glossarylink" href="glossary.html#agreement">agreements and transfers</a> that you or an
+<a class="glossarylink" href="glossary.html#author">author or artist</a> entered into
+before being able to assess the true market for the work.  Specifically, this tool
+can assist you in identifying whether you may have a right to terminate an agreement
+or transfer and if so, it calculates, based on the information you provide, whether
+you may be to activate your termination right at the moment.  In addition, this tool
+gathers some basic details about the creation and exploitation
+of the work that can be passed to either a lawyer or a
+<a class="glossarylink" href="glossary.html#clinic">participating law clinic</a>
+that can then represent you in effecting your termination rights.</p>
+
+<p>To learn more about the information that you will need in order to complete the tool,
+<a href="what_information_do_i_need.php">click here</a>.</p>
+
+<h3 id="get_started">How do I get started?</h3>
+
+<p>To start working through the tool and see if you may be eligible to terminate an
+agreement entered into in relation to a creative work and, if so, whether your
+"notice window" is currently open, <a href="termination.php">click here</a>.</li>
+</p>
+<a style="color:black;" href="termination.php"><p style="text-align:center; border:1px solid black; width:90px; margin-right:auto; margin-left:auto; background-color:grey;">
+START</p></a>
+<p><b>Please note, this tool is not, nor is it intended to be, legal advice, or a substitute for legal advice.</b>
+</div>
+
+</div>
+
+<? include_once "_footer.php"; ?>
