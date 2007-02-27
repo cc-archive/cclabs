d4a482cd2044721e175b15dae9a89b40ce5f8ea5
kidproto <kidproto@committer.creativecommons.org>
2007-02-27 19:35:28 +0000

    Fixed it. Also, added required fields...

--- feedback.php
+++ feedback.php
@@ -4,23 +4,33 @@ echo('<h3 align="center">');
 
 $ref = $_SERVER["HTTP_REFERER"];
 
-if (strpos($ref,'http://labs.creativecommons.org/') === 0
-    || strpos($ref,'http://labt.creativecommons.org/') === 0)
+// echo '<pre>';
+// print_r($_REQUEST);
+// echo '<pre>';
+
+if ( strpos($ref,'http://labs.creativecommons.org/') === 0
+     || strpos($ref,'http://labt.creativecommons.org/') === 0
+     &&
+     !empty($_REQUEST['compare']) && !empty($_REQUEST['understandable'])
+     && !empty($_REQUEST['comments']) )
 {
-    $email      = $HTTP_POST_VARS[email];
-    $mailto     = "labs@creativecommons.org";
+    if ( empty($_REQUEST['email'] ) )
+        $email = 'noreply@creativecommons.org';
+    else 
+        $email = $_REQUEST['email']; 
+
+    // $mailto     = "labs@creativecommons.org";
+    $mailto     = "jon@creativecommons.org";
     $mailsubj   = "Labs Feedback";
     $mailhead   = "From: $email\n";
     
-    reset ($HTTP_POST_VARS);
-    
-    $mailbody   = "Submitted from " . $ref . "\n";
+    $mailbody   = "Submitted from $ref \n";
     
     while (list ($key, $val) = each ($HTTP_POST_VARS)) 
     { 
     	$mailbody .= "$key : $val\n"; 
     }
-    
+
     if (!eregi("\n",$HTTP_POST_VARS[email])) 
     { 
         mail($mailto, $mailsubj, $mailbody, $mailhead); 
@@ -29,7 +39,10 @@ if (strpos($ref,'http://labs.creativecommons.org/') === 0
 } 
 else 
 {
-    echo('Thranks for the feedback!');
+    echo('<span style="color:red">' . 
+         'Use must give feedback to submit this form. ' . 
+         'Please use your browser\'s back arrow to fill out required fields.' .
+         '</span>');
 }
 echo('</h3>');
 
