fbd5ccb537fe5bb764c13c3d0f4b7d93f9a6b541
mlinksva <mlinksva@committer.creativecommons.org>
2007-04-08 21:00:45 +0000

    /policies is on cc.org

--- _footer.php
+++ _footer.php
@@ -90,7 +90,7 @@ licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2
       </div>
       <p>
 <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img src="http://i.creativecommons.org/l/by/3.0/88x31.png" alt="Creative Commons License" style="border: medium none ;" height="31" width="88"></a><br>
-Except where otherwise <a class="subfoot" href="/policies#license">noted</a>, content on this site is<br/>
+Except where otherwise <a class="subfoot" href="http://creativecommons.org/policies#license">noted</a>, content on this site is<br/>
 licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/" class="subfoot">Creative Commons Attribution 3.0 License</a>
       <p>
     </div>
